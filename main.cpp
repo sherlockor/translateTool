8cd966940421eb8e2f115dc2b634852c255b9035
sherlock <sherlock_lin@163.com>
2020-03-19 22:32:15 +0800

    删掉qt依赖

--- main.cpp
+++ main.cpp
@@ -4,13 +4,11 @@
 #include "translate_factory.h"
 #include "google_translate.h"
 #include "youdao_translate.h"
-#include <QDebug>
-#include <QString>
 using namespace std;
 
 void PrintResutl(const std::string& response, void* userData)
 {
-    qDebug()<<QString::fromStdString(response);
+    std::cout<<response<<endl;
 }
 
 int main(void)
@@ -24,7 +22,7 @@ int main(void)
 
     std::shared_ptr<BaseTranslate> youdaoTranslate = factory.GetTranslateInstance(TranslateFactory::Youdao);
     youdaoTranslate->SetCallback(&PrintResutl, NULL);
-    youdaoTranslate->Translate("我男朋友很帅");
+    youdaoTranslate->Translate("支持全球27个语种的高质量互译服务");
 
     BaseTranslate::Release();
 
